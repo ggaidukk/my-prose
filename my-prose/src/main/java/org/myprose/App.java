a4517ab632dc7ca64467ab9bd9b041206b1e7bdd
gaidukkk <gaiduk.elizaveta@gmail.com>
2025-06-15 20:43:50 +0200

    Added a license header for the license at the top of each source code file

---
+++ my-prose/src/main/java/org/myprose/App.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2025 Yelyzaveta 
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+
+package org.myprose;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ my-prose/src/test/java/org/myprose/AppTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2025 Yelyzaveta 
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package org.myprose;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+        extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+}
